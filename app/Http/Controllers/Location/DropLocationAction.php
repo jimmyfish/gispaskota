ea67d7071d887d6f96f477b275e69fecd10d60cc
Dito YP <killcoder212@gmail.com>
2021-05-20 17:30:07 +0700

    add edit and delete

--- app/Http/Controllers/Location/DropLocationAction.php
+++ app/Http/Controllers/Location/DropLocationAction.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Location;
 
 use App\Http\Controllers\Controller;
+use App\Models\Location;
 use Illuminate\Http\Request;
 
 class DropLocationAction extends Controller
@@ -10,5 +11,13 @@ class DropLocationAction extends Controller
     public function do(Request $request, $id)
     {
         if (!is_numeric($id)) return redirect()->route('dashboard.location');
+
+        $data = Location::find($id);
+
+        if (!$data) return redirect()->route('dashboard.location');
+
+        $data->delete();
+
+        return redirect()->route('dashboard.location', ['message' => 'Successfully deleted!']);
     }
 }
--- app/Http/Controllers/Location/EditLocationAction.php
+++ app/Http/Controllers/Location/EditLocationAction.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Controllers\Location;
 
-use App\Http\Controllers\Controller;
+use App\Models\Location;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class EditLocationAction extends Controller
 {
-    //
+    public function put(Request $request, $id)
+    {
+        if (!is_numeric($id)) return redirect()->route('dashboard.location');
+
+        $original = Location::find($id);
+
+        if (!$original) return redirect()->route('dashboard.location');
+
+        $data = $request->only([
+            'title',
+            'address',
+            'description',
+            'operation_hours',
+            'price_range',
+            'geolocation',
+            'category_id',
+        ]);
+
+        foreach ($data as $key => $datum) {
+            $original->{$key} = $datum;
+        }
+
+        $original->save();
+
+        return redirect()->route('dashboard.location', ['message' => 'Data successfully updated!']);
+    }
 }
